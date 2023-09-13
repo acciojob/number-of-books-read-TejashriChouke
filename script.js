d01a13f269c65b6cc688e57bb3e371f8fbe996ea
TejashriChouke <139746868+TejashriChouke@users.noreply.github.com>
2023-09-13 23:10:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
+function numberOfBooksRead(library) {
+  // Check if the library object has a 'books' property, and if it's an array
+  if (library && library.books && Array.isArray(library.books)) {
+    // Use the filter method to count books with readingStatus set to true
+    const readBooks = library.books.filter(book => book.readingStatus === true);
+    return readBooks.length;
+  } else {
+    // Return 0 if the library object or 'books' property is missing or not an array
+    return 0;
+  }
+}
 
-const numberOfBooksRead = () => {
-  // write your code here
+// Example library object
+const library = {
+  books: [
+    {
+      author: "Author 1",
+      title: "Book 1",
+      readingStatus: true,
+    },
+    {
+      author: "Author 2",
+      title: "Book 2",
+      readingStatus: false,
+    },
+    {
+      author: "Author 3",
+      title: "Book 3",
+      readingStatus: true,
+    },
+  ],
 };
 
-// Do not change the code below
-
-alert(numberOfBooksRead());
+// Call the function to get the number of books read
+const numBooksRead = numberOfBooksRead(library);
+console.log("Number of books read:", numBooksRead); // Output: 2
